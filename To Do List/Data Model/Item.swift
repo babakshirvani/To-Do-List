3d9056baa6314c07944c203a095bf96db0c73ca7
babakshirvani <35647198+babakshirvani@users.noreply.github.com>
2019-04-07 17:59:03 -0400

    Convert to MVC Design pattern.

---
+++ To Do List/Data Model/Item.swift
@@ -0,0 +1,19 @@
+//
+//  item.swift
+//  To Do List
+//
+//  Created by Babak Shirvani on 2019-04-07.
+//  Copyright © 2019 Babak Shirvani. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done: Bool = false
+    
+    
+    
+    
+}
